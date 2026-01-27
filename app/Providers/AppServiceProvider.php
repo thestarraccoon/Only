7c9e6fa5bd6399551ea0c2b10574a62a4a1cf6e9
Максим <thestarraccoon@gmail.com>
2026-01-27 22:12:09 +0400

    pipi pupu

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Services\RegisterUserService;
-use App\Services\RoleResolverService;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
