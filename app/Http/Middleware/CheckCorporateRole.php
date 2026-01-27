31da1d15f21fb5c5889e45d86630b430fae5f978
Максим <thestarraccoon@gmail.com>
2026-01-28 01:43:59 +0400

    pipi pupu

--- app/Http/Middleware/CheckCorporateRole.php
+++ app/Http/Middleware/CheckCorporateRole.php
@@ -5,15 +5,29 @@ namespace App\Http\Middleware;
 use Closure;
 use App\Enums\RoleConfig;
 use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\Request;
 
 class CheckCorporateRole
 {
-    public function handle(Request $request, Closure $next, string ...$allowedRoles): mixed
+    public function handle(Request $request, Closure $next): mixed
     {
         $corporateId = $request->header('X-Corporate-ID');
 
         if (!$corporateId) {
+            throw new AuthorizationException(__('auth.access_denied'));
+        }
+
+        $user = $request->user();
+        if (!$user) {
+            throw new AuthenticationException(__('auth.unauthorized'));
+        }
+
+        $corporateRole = RoleConfig::fromCorporateId($corporateId);
+
+        $userRole = RoleConfig::fromPositionId($user->position_id);
+
+        if ($corporateRole !== $userRole) {
             throw new AuthorizationException(__('auth.access_denied'));
         }
 
